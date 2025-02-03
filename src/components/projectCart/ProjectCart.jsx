997bf186591c77d784d9cefce359a07921cc05a8
Vikash <vikashparjapati59@gmail.com>
2025-02-03 10:20:52 +0530

    update restaurantimage in Projects

--- src/components/projectCart/ProjectCart.jsx
+++ src/components/projectCart/ProjectCart.jsx
@@ -3,15 +3,12 @@ import React from "react";
 export const ProjectCart = (props) => {
   return (
     <div className="bg-lightGray rounded-lg shadow-lg overflow-hidden  w-full max-w-md mx-auto">
-      {/* Image Section */}
-      <div className="h-48 md:h-64 bg-cover bg-center" style={{ backgroundImage: `url(${props.image})` }}>
-      </div>
-
-      {/* Text Section */}
+      <div className="flex items-center justify-center m-2">
+        <img src={props.image} alt={props.title} className="h-48 md:h-64 w-auto   border border-transparent rounded-lg "/></div>
       <div className="p-2">
         <h5 className="text-lg font-bold text-black">{props.title}</h5>
         <p className="text-gray-200 mt-2">{props.description}</p>
       </div>
-    </div>
+     </div>  
   );
 };
